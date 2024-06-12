71387913f1b3bfcfcbf2cf6e8ab4722a81e04a8d
Soufiane-Boukhar <boukhar.soufiane.coding@gmail.com>
2024-06-12 11:06:34 -0400

    save work

--- api/addContact.ts
+++ api/addContact.ts
@@ -1,6 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import mysql from 'mysql';
-import bodyParser from 'body-parser';
 
 const db = mysql.createConnection({
   host: 'mysql-1fb82b3b-boukhar-d756.e.aivencloud.com',
@@ -23,27 +22,32 @@ db.connect(err => {
 });
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { nom_complete, tel, email, sujet, message } = req.body;
+  try {
+    const { nom_complete, tel, email, sujet, message } = req.body;
 
-  if (!nom_complete || !tel || !email || !sujet || !message) {
-    console.error('One or more required fields are null.');
-    return res.status(400).send('One or more required fields are null.');
-  }
-
-  const sqlInsert = 'INSERT INTO contacts (full_name, tel, email, subject, message) VALUES (?, ?, ?, ?, ?)';
-
-  db.query(sqlInsert, [nom_complete, tel, email, sujet, message], (err, result) => {
-    if (err) {
-      console.error('Error inserting data into contacts table:', err);
-      return res.status(500).send('An error occurred while inserting data into contacts table.');
+    if (!nom_complete || !tel || !email || !sujet || !message) {
+      console.error('One or more required fields are null.');
+      return res.status(400).send('One or more required fields are null.');
     }
 
-    console.log('Data inserted successfully into contacts table.');
-    res.status(200).send('Data inserted successfully into contacts table.');
-  });
+    const sqlInsert = 'INSERT INTO contacts (full_name, tel, email, subject, message) VALUES (?, ?, ?, ?, ?)';
 
-  // Set CORS headers to allow requests from any origin
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    db.query(sqlInsert, [nom_complete, tel, email, sujet, message], (err, result) => {
+      if (err) {
+        console.error('Error inserting data into contacts table:', err);
+        return res.status(500).send('An error occurred while inserting data into contacts table.');
+      }
+
+      console.log('Data inserted successfully into contacts table.');
+      res.status(200).send('Data inserted successfully into contacts table.');
+    });
+
+    // Set CORS headers to allow requests from any origin
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  } catch (error) {
+    console.error('Error in handler:', error);
+    res.status(500).send('Internal Server Error');
+  }
 }
