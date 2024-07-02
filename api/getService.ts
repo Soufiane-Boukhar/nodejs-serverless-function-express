efb39bb758ff378595b66d213c90c6f7de6133dc
Soufiane-Boukhar <boukhar.soufiane.coding@gmail.com>
2024-07-02 12:59:49 +0100

    backend add services select

---
+++ api/getService.ts
@@ -0,0 +1,49 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import mysql from 'mysql';
+
+const db = mysql.createConnection({
+  host: 'mysql-1fb82b3b-boukhar-d756.e.aivencloud.com',
+  port: 20744,
+  user: 'avnadmin',
+  password: 'AVNS_wWoRjEZRmFF5NgjGCcY',
+  database: 'defaultdb',
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectTimeout: 10000 
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('MySQL connected...');
+});
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  if (req.method === 'GET') {
+    const sqlSelect = 'SELECT * FROM services';
+
+    db.query(sqlSelect, (err, results) => {
+      if (err) {
+        console.error('Error executing SELECT query:', err);
+        res.status(500).json({ error: 'Database query error', details: err.message });
+        return;
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json({ contacts: results });
+    });
+  } else {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
+}
