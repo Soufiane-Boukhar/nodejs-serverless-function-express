d1561921f9efc6d6ed3f02741ae099c69f8d5c53
Soufiane-Boukhar <boukhar.soufiane.coding@gmail.com>
2024-07-02 11:23:35 +0100

    backend add contact select

---
+++ api/excelContact.ts
@@ -0,0 +1,68 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import mysql from 'mysql';
+import ExcelJS from 'exceljs';
+
+const db = mysql.createConnection({
+  host: 'mysql-1fb82b3b-boukhar-d756.e.aivencloud.com',
+  port: 20744,
+  user: 'avnadmin',
+  password: 'AVNS_wWoRjEZRmFF5NgjGCcY',
+  database: 'defaultdb',
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectTimeout: 10000 
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('MySQL connected...');
+});
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  if (req.method === 'POST') {
+    const sqlSelect = 'SELECT * FROM contacts';
+
+    db.query(sqlSelect, async (err, results) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        res.status(500).json({ error: 'Database query error' });
+        return;
+      }
+
+      const workbook = new ExcelJS.Workbook();
+      const worksheet = workbook.addWorksheet('Contacts');
+
+      worksheet.columns = [
+        { header: 'ID', key: 'id', width: 10 },
+        { header: 'Name', key: 'name', width: 30 },
+        { header: 'Email', key: 'email', width: 30 },
+        { header: 'Phone', key: 'phone', width: 20 }
+      ];
+
+      results.forEach(result => {
+        worksheet.addRow(result);
+      });
+
+      res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+      res.setHeader('Content-Disposition', 'attachment; filename=contacts.xlsx');
+
+      const buffer = await workbook.xlsx.writeBuffer();
+      res.status(200).send(buffer);
+    });
+  } else {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
+}
---
+++ api/getContact.ts
@@ -0,0 +1,50 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import mysql from 'mysql';
+
+const db = mysql.createConnection({
+  host: 'mysql-1fb82b3b-boukhar-d756.e.aivencloud.com',
+  port: 20744,
+  user: 'avnadmin',
+  password: 'AVNS_wWoRjEZRmFF5NgjGCcY',
+  database: 'defaultdb',
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectTimeout: 10000 
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('MySQL connected...');
+});
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  if (req.method === 'POST') {
+    const sqlSelect = 'SELECT * FROM contacts';
+
+    db.query(sqlSelect, (err, results) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        res.status(500).json({ error: 'Database query error' });
+        return;
+      }
+
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json({ contacts: results });
+    });
+  } else {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
+}
